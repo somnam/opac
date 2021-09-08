1ac10fff0dff218227c8226b9dcabbf881264b51
Tomek Miodek <tomek.miodek@gmail.com>
2021-09-08 20:48:40 +0200

    Fixing schedule delayed jobs

--- server/src/entrypoints/tasks/worker.py
+++ server/src/entrypoints/tasks/worker.py
@@ -1,7 +1,8 @@
-import rq
-import redis
 import logging.config
 
+import redis
+import rq
+import rq.scheduler
 from src.config import Config
 
 config = Config()
@@ -16,7 +17,23 @@ def run() -> None:
 
     connection = redis.Redis(connection_pool=connection_pool)
 
-    with rq.Connection(connection):
-        queues = config.getstruct("rq", "queues")
+    queues = config.getstruct("rq", "queues")
+    result_ttl = config.getint("rq", "result_ttl")
+    logging_level = config.get("logger_rq", "level")
 
-        rq.Worker(queues=queues).work(with_scheduler=True)
+    # Release scheduler locks before starting a new worker.
+    rq.scheduler.RQScheduler(
+        queues=queues,
+        connection=connection,
+        logging_level=logging_level,
+    ).release_locks()
+
+    # Start worker with scheduler.
+    rq.Worker(
+        queues=queues,
+        connection=connection,
+        default_result_ttl=result_ttl,
+    ).work(
+        with_scheduler=True,
+        logging_level=logging_level,
+    )
